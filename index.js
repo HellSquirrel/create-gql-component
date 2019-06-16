f80b8613199c8225cb6b9cc719803b724b6e9e35
HellSquirrel <zloymult@gmail.com>
2019-06-17 00:30:16 +0300

    Add styles for Library component

--- index.js
+++ index.js
@@ -37,18 +37,18 @@ function writeToFile(name, getData = () => "") {
 
 const toLower = str => `${str.charAt(0).toLowerCase()}${str.slice(1)}`;
 
-writeToFile("operations.graphql");
-writeToFile(
-  "index.js",
-  componentName => `import React from 'react';
-import cs from './styles';
+// writeToFile("operations.graphql");
+// writeToFile(
+//   "index.js",
+//   componentName => `import React from 'react';
+// import cs from './styles';
 
-const ${componentName} = () => (
-  <div className={cs.${toLower(componentName)}}>👋</div>
-);
+// const ${componentName} = () => (
+//   <div className={cs.${toLower(componentName)}}>👋</div>
+// );
 
-export default ${componentName};`
-);
+// export default ${componentName};`
+// );
 
 const getStylesContent = componentName =>
   styles[componentName]
